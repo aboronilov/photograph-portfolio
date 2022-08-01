45b70203cc8b7df910d6a371dc3a903e552a0e5f
Anatoly <a.boronilov@infoculture.ru>
2022-08-01 16:52:05 +0200

    header css

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ window.addEventListener("scroll", ()=>{
     const nav = document.querySelector("nav");
     nav.classList.toggle("window-scrolled", window.scrollY > 0)
 })
+
+// contact buttons circular text
+
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+    let insert = text.innerHTML.split('').map((letter, index) => `<span style="transform: rotate(${index * 12}deg)">${letter}</span>`).join();
+    const final = insert.replace(/,/g, '')
+    text.innerHTML = final
+
+})
