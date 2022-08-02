994a94d39ae9dcfaaf8f97fa00f73f2b23419a3e
Anatoly <a.boronilov@infoculture.ru>
2022-08-02 12:21:33 +0200

    slider

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ textButtons.forEach(textButton => {
     text.innerHTML = final
 
 })
+
+// swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpionts: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+        }
+    }
+  });
